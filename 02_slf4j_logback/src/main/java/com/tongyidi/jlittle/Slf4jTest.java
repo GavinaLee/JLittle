c91542e59ecf476d538d7fd396afa575c0bc508c
wblixq <itlixq@yonyou.com>
2018-10-30 15:10:16 +0800

    slf4j+logback 使用

---
+++ 02_slf4j_logback/src/main/java/com/tongyidi/jlittle/Slf4jTest.java
@@ -0,0 +1,20 @@
+package com.tongyidi.jlittle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ *
+ */
+public class Slf4jTest {
+
+	private final static Logger logger = LoggerFactory.getLogger(Slf4jTest.class);
+
+	public static void main(String[] args) {
+		logger.debug("this is debug");
+		logger.info("this is info");
+		logger.warn("this is warn");
+		logger.error("this is error");
+	}
+}
